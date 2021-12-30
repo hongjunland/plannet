9f5168175d335fa26c6eba5168f4db2dd1e4212e
이홍준 <zunza96@naver.com>
2021-12-31 00:26:34 +0900

    init

---
+++ src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+//        Info info = new Info();
+        return new OpenAPI().components(new Components());
+    }
+}
