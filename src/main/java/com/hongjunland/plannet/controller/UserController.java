e7b3e3fe898033b85b9bbdcff8416f2b5285f7db
이홍준 <zunza96@naver.com>
2022-01-07 23:02:17 +0900

    create Board.java, BoardController.java, BoardDto.java, BoardRepository.java

--- src/main/java/com/hongjunland/plannet/controller/UserController.java
+++ src/main/java/com/hongjunland/plannet/controller/UserController.java
@@ -6,15 +6,10 @@ import com.hongjunland.plannet.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
